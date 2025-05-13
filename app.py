a59a2cd7fab4ec40edd3b841f294588b806d6ff9
ZalamakwanaSheshang <sheshang4707@gmail.com>
2025-05-13 15:54:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding="ISO-8859-1")
 else:
-    os.chdir(r"https://github.com/ZalamakwanaSheshang/Streamlit_Superstore_Dashboard/blob/main/superstore.csv")
     df = pd.read_csv("superstore.csv", encoding="ISO-8859-1")
 
 col1, col2 = st.columns((2))
